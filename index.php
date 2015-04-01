2687722ad4a44035c77071516260091f900ab507
Janne Klouman <janne@klouman.com>
2015-04-01 22:09:40 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+
+    <head>
+
+        <!-- Reset stylesheet -->
+        <link rel="stylesheet" href="/resource/style/reset.css"/>
+
+        <!-- Main stylesheet -->
+        <link rel="stylesheet" href="/resource/style/main.css"/>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="application-name" content="colorhelper">
+
+    </head>
+
+    <body>
+
+    <!-- Header -->
+    <header>
+
+        <!-- TODO:: put colors here -->
+
+    </header>
+
+    <!-- Content wrap -->
+    <div role="main" class="main-wrapper">
+
+        <!-- Main content -->
+        <div id="main-content">
+
+
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <!-- Navigation -->
+    <div id="nav">
+
+
+    </div>
+
+    </body>
+
+</html>
